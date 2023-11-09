8c8b9dab62f01c22e58b6845408dd24055c4909c
Yuya <94564374+yt27828@users.noreply.github.com>
2023-11-08 22:06:10 -0500

    Initial commit for Yuya's branch

---
+++ team_india_project/team_india_project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  team_india_project
+//
+//  Created by Yuya Taniguchi on 11/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
