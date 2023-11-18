dadf6b959e46acc6ea6f6f5e71c0e755b5b70545
Yong lin <YL4648@Columbia.edu>
2023-11-18 00:11:48 -0500

    user struct and population from database

--- Travel Buds/Travel Buds/TableViewController.swift
+++ Travel Buds/Travel Buds/TableViewController.swift
@@ -4,13 +4,36 @@
 //
 //  Created by Yongkang Lin on 11/8/23.
 //
-
-import Foundation
 import UIKit
+import Foundation
+import Firebase
+import FirebaseAuth
+import FirebaseStorage
 
 class TableViewController: UITableViewController{
     
     var trips: [String] = []
+    let user: User? = nil
+    
+    class FirebaseManager: NSObject {
+        let auth: Auth
+        let storage: Storage
+        let firestore: Firestore
+        
+        static let shared = FirebaseManager()
+        
+        override init(){
+            self.auth = Auth.auth()
+            self.storage = Storage.storage()
+            self.firestore = Firestore.firestore()
+            super.init()
+        }
+    }
+    
+    override func viewDidLoad(){
+        self.populateData()
+        super.viewDidLoad()
+    }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return trips.count
@@ -29,5 +52,36 @@ class TableViewController: UITableViewController{
                 }
             }
         }
+    }
+    
+    func populateData(){
+        let uid = FirebaseManager.shared.auth.currentUser?.uid ?? ""
+        let userRef = FirebaseManager.shared.firestore.collection("users").document(uid)
+        userRef.getDocument { (document, error) in
+            if let document = document, document.exists {
+                let userData = document.data(),
+                    user = User(
+                        email: userData!["email"] as? String ?? "",
+                        userName: userData!["userName"] as? String ?? "",
+                        firstName: userData!["firstName"] as? String ?? "",
+                        lastName: userData!["lastName"] as? String ?? "",
+                        trips: (userData!["trips"] as? [[String: Any]] ?? []).compactMap { tripData in
+                            return Trip(
+                                tripID: tripData["tripID"] as? String ?? "",
+                                chatID: tripData["chatID"] as? String,
+                                location: tripData["location"] as? String ?? "",
+                                interest: tripData["interest"] as? String ?? "",
+                                arrival: tripData["arrival"] as? Date ?? Date(),
+                                departure: tripData["departure"] as? Date ?? Date()
+                            )
+                        }
+                        )
+                    }
+        }
+        print(user?.email)
+        print(user?.userName)
+        print(user?.firstName)
+        print(user?.lastName)
+        print(user?.trips)
     }
 }
