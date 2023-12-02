aee321e48b99cd49614834640485b2647760f597
Brennen Yu <bsy2106@columbia.edu>
2023-12-02 18:10:21 -0500

    Testing/WIP  Cloud Functions.

--- server_cloud_functions/functions/index.js
+++ server_cloud_functions/functions/index.js
@@ -1,5 +1,5 @@
 // The Cloud Functions for Firebase SDK to create Cloud Functions and triggers.
-const {logger} = require("firebase-functions/logger");
+const {logger} = require("firebase-functions");
 const {setGlobalOptions} = require("firebase-functions/v2/options");
 const {onRequest} = require("firebase-functions/v2/https");
 // const {onDocumentWritten} = require("firebase-functions/v2/firestore");
@@ -14,28 +14,32 @@ const {getFirestore} = require("firebase-admin/firestore");
 initializeApp();
 setGlobalOptions({maxInstances: 10, region: "us-east1"});
 
-exports.helloWorld = onRequest((req, res) => {
+exports.helloWorld = onRequest({timeoutSeconds: 1}, (req, res) => {
   logger.info("Hello logs!", {structuredData: true});
-  res.send("Hello from Firebase!");
+  return res.send("Hello from Firebase!");
 });
 
 // Add functionality where user can not make multiple
 // of same trip. This is easy if they are in a group for trip already.
 // Might need to track pending as well?
-exports.makeGroup = onRequest(async (req, res) => {
+exports.makeGroup = onRequest({timeoutSeconds: 1}, (req, res) => {
   // if (req.auth != true) {
   //   res.status(401).send("Unauthorized:" +
   //       "Client failed to authenticate with the server.");
   //   return ""
   // }
-
-  const myUserId = req.body.uid;
-  const myWeekStartDate = req.body.weekStartDate;
-  const myWeekEndDate = req.body.weekEndDate;
-  const myDest = req.body.destination;
-  const myInterest = req.body.interest;
+  try {
+    const myUserId = req.body.uid;
+    const myWeekStartDate = req.body.weekStartDate;
+    const myWeekEndDate = req.body.weekEndDate;
+    const myDest = req.body.destination;
+    const myInterest = req.body.interest;
+  }
+  catch (error){
+    return res.status(400).send("Bad Request")
+  }
   // Insert logic so no overlapping also time logic
-  const tripsSnapshot = await getFirestore()
+  const tripsSnapshot = getFirestore()
       .collection("pending")
       .where(myWeekStartDate, "==", "weekStartDate")
       .where(myDest, "==", "destination")
@@ -45,18 +49,24 @@ exports.makeGroup = onRequest(async (req, res) => {
       .get();
 
   if (tripsSnapshot.size != 4) {
-    res.status(500).send("Placeholder error.");
-    return;
+    const myTripDoc = getFirestore().collection("pending").add({
+      weekStartDate: myWeekStartDate,
+      weekEndDate: myWeekEndDate,
+      destination: myDest,
+      interest: myInterest,
+      userId: myUserId,
+    });
+    return res.status(201).send(`Created pending trip: ${myTripDoc.id}`);
   }
 
   const myMembers = [];
   tripsSnapshot.forEach((tripDoc) => {
     myMembers.push(tripDoc.get("userId"));
     tripDoc.ref.delete();
-    logger.log("deleted trip: ${tripDoc.id}");
+    logger.log(`Deleted pending trip: ${tripDoc.id}`);
   });
 
-  const groupDoc = await getFirestore().collection("groups").add({
+  const myGroupDoc = getFirestore().collection("groups").add({
     weekStartDate: myWeekStartDate,
     weekEndDate: myWeekEndDate,
     destination: myDest,
@@ -64,6 +74,5 @@ exports.makeGroup = onRequest(async (req, res) => {
     members: myMembers,
   });
 
-  res.status(201).send("Group created with id: ${groupDoc.id}");
-  return;
+  return res.status(201).send(`Group created with id: ${myGroupDoc.id}`);
 });
