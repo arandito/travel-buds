bd7ba978cd63f70efa36d351de77b2803dde112d
brennenyu <bsy2106@columbia.edu>
2023-12-02 23:09:01 -0500

    makeGroup() working via GCP & Thunderclient.

--- server_cloud_functions/functions/index.js
+++ server_cloud_functions/functions/index.js
@@ -8,13 +8,16 @@ const {onRequest} = require("firebase-functions/v2/https");
 const {initializeApp} = require("firebase-admin/app");
 const {getFirestore} = require("firebase-admin/firestore");
 
+// Request body-parser.
+const jsonParser = require("body-parser").json();
+
 // To set up scheduled Cloud Functions.
 // const {onSchedule} = require{"firebase-functions/v2/scheduler"};
 
 initializeApp();
 setGlobalOptions({maxInstances: 10, region: "us-east1"});
 
-exports.helloWorld = onRequest({timeoutSeconds: 1}, (req, res) => {
+exports.helloWorld = onRequest({timeoutSeconds: 1, cors: true}, (req, res) => {
   logger.info("Hello logs!", {structuredData: true});
   return res.send("Hello from Firebase!");
 });
@@ -22,34 +25,47 @@ exports.helloWorld = onRequest({timeoutSeconds: 1}, (req, res) => {
 // Add functionality where user can not make multiple
 // of same trip. This is easy if they are in a group for trip already.
 // Might need to track pending as well?
-exports.makeGroup = onRequest({timeoutSeconds: 1}, (req, res) => {
+exports.makeGroup =
+onRequest({timeoutSeconds: 3, cors: true}, async (req, res) => {
   // if (req.auth != true) {
   //   res.status(401).send("Unauthorized:" +
   //       "Client failed to authenticate with the server.");
   //   return ""
   // }
+
+  jsonParser(req, res, (err) => {
+    if (err) {
+      return res.status(500).send(err.message);
+    }
+  });
+
+  // Catches error if incorrectly formatted request.
+  let myUserId;
+  let myWeekStartDate;
+  let myWeekEndDate;
+  let myDest;
+  let myInterest;
+
   try {
-    const myUserId = req.body.uid;
-    const myWeekStartDate = req.body.weekStartDate;
-    const myWeekEndDate = req.body.weekEndDate;
-    const myDest = req.body.destination;
-    const myInterest = req.body.interest;
+    myUserId = req.body.uid;
+    myWeekStartDate = req.body.weekStartDate;
+    myWeekEndDate = req.body.weekEndDate;
+    myDest = req.body.destination;
+    myInterest = req.body.interest;
+  } catch (error) {
+    return res.status(400).send(error);
   }
-  catch (error){
-    return res.status(400).send("Bad Request")
-  }
-  // Insert logic so no overlapping also time logic
-  const tripsSnapshot = getFirestore()
+  // Insert body type check
+  const tripsSnapshot = await getFirestore()
       .collection("pending")
       .where(myWeekStartDate, "==", "weekStartDate")
-      .where(myDest, "==", "destination")
-      .where(myInterest, "==", "interest")
-      .where(myUserId, "!=", "userId")
+      .where("destination", "==", myDest)
+      .where("interest", "==", myInterest)
       .limit(4)
       .get();
 
   if (tripsSnapshot.size != 4) {
-    const myTripDoc = getFirestore().collection("pending").add({
+    const myTripDoc = await getFirestore().collection("pending").add({
       weekStartDate: myWeekStartDate,
       weekEndDate: myWeekEndDate,
       destination: myDest,
@@ -60,13 +76,15 @@ exports.makeGroup = onRequest({timeoutSeconds: 1}, (req, res) => {
   }
 
   const myMembers = [];
+
+  myMembers.push(myUserId);
   tripsSnapshot.forEach((tripDoc) => {
     myMembers.push(tripDoc.get("userId"));
     tripDoc.ref.delete();
     logger.log(`Deleted pending trip: ${tripDoc.id}`);
   });
 
-  const myGroupDoc = getFirestore().collection("groups").add({
+  const myGroupDoc = await getFirestore().collection("groups").add({
     weekStartDate: myWeekStartDate,
     weekEndDate: myWeekEndDate,
     destination: myDest,
