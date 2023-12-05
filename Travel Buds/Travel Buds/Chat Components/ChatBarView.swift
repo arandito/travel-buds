64ff4e61ca6828e3823f186c390d067e0e5257ba
Yuya <94564374+yt27828@users.noreply.github.com>
2023-12-04 20:10:55 -0500

    Restructured cvm pass around and updated auto scroll and text editor

--- Travel Buds/Travel Buds/Chat Components/ChatBarView.swift
+++ Travel Buds/Travel Buds/Chat Components/ChatBarView.swift
@@ -1,44 +1,78 @@
-//
-//  ChatBarView.swift
-//  Travel Buds
-//
-//  Created by Yuya Taniguchi on 12/3/23.
-//
-
 import SwiftUI
+import Combine
 
 struct ChatBarView: View {
     
-    @ObservedObject var cvm: ChatViewModel
+    @EnvironmentObject private var cvm: ChatViewModel
+    @State private var keyboardHeight: CGFloat = 0
     
     var body: some View {
         HStack {
+            
             Image(systemName: "photo.on.rectangle")
                 .font(.system(size: 24))
-                .foregroundColor(Color(.darkGray))
+                .foregroundColor(Color.purple)
             
-            TextEditor(text: $cvm.chatText)
-                .padding(8)
-                .background(
-                    RoundedRectangle(cornerRadius: 8)
-                        .stroke(Color(.darkGray), lineWidth: 1)
-                )
-                .frame(maxHeight: 40)
-                .padding()
+            CustomTextField(placeholder: Text("Enter your message here"), text: $cvm.chatText)
             
             Button {
                 cvm.handleSend()
             } label: {
                 Image(systemName: "paperplane.fill")
                     .foregroundColor(.white)
+                    .padding(10)
+                    .background(Color.purple)
+                    .cornerRadius(50)
             }
-            .padding(.horizontal)
-            .padding(.vertical, 8)
-            .background(Color.purple)
-            .cornerRadius(8)
         }
         .padding(.horizontal)
-        .padding(.vertical, 8)
+        .padding(.vertical, 10)
+        .background(Color("Gray"))
+        .cornerRadius(50)
+        .padding()
+
+    }
+}
+/*
+struct ChatBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        let cvm = ChatViewModel(groupId: "Group2")
+        ChatView(cvm: cvm)
+        return ChatBarView()
+    }
+}
+*/
+
+struct CustomTextField: View {
+    var placeholder = Text("Enter here...")
+    @Binding var text: String
+    var editingChanged: (Bool) -> () = {_ in}
+    var commit: () -> () = {}
+    
+    var body: some View {
+        ZStack(alignment: .leading) {
+            
+            TextEditor(text: $text)
+                .padding(4)
+                .frame(maxHeight: 50)
+                .onTapGesture {
+                    if text.isEmpty {
+                        text = ""
+                    }
+                }
+            
+            if text.isEmpty {
+                Text("Enter here...")
+                    .foregroundColor(Color.gray)
+
+            }
+
+        }
+        .onReceive(Just(text)) { _ in
+            if text.isEmpty {
+                text = ""
+            }
+        }
     }
 }
 
